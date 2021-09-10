7947cb7655bcf4d3efaaf0219561cddd8b6fe53c
My PC <you@example.com>
2021-09-10 15:17:40 +0700

    nhan

---
+++ bài 5 html form và table/Bài tập/bt form đơn giản/toán tử.js
@@ -0,0 +1,9 @@
+var inputWidth;
+var inputHeight;
+inputWidth = prompt('Nhập chiều dài')
+inputHeight = prompt('Nhập chiều rộng')
+
+var width = parseInt(inputWidth)
+var height = parseInt(inputHeight)
+var area = inputHeight*inputWidth
+document.write('S = ' + area)
